c3d71acd9c840076dca8a7b87373478b3d6563c8
Fikir Demeke <fikirgenizele@gmail.com>
2025-10-20 20:10:20 -0400

    Added contents for thermostat.java class

--- org/howard/edu/lsp/midterm/question4/Thermostat.java
+++ org/howard/edu/lsp/midterm/question4/Thermostat.java
@@ -2,10 +2,52 @@
  * Course: Large Scale Programming 
  * Date: October 20 2025
  * Midterm Exam
+ * Thermostat class
  */
 
 package org.howard.edu.lsp.midterm.question4;
 
-public class Thermostat {
-    
+//extends device and implements networked
+public class Thermostat extends Device implements Networked {
+  private double temperatureC;
+
+  // Constructor
+  public Thermostat(String id, String location, double initialTempC) {
+    super(id, location);
+    this.temperatureC = initialTempC;
+  }
+
+  // Accessors
+  public double getTemperatureC() {
+    return temperatureC;
+  }
+
+  public void setTemperatureC(double temperatureC) {
+    this.temperatureC = temperatureC;
+  }
+
+  // Networked implementation
+  @Override
+  public void connect() {
+    setConnected(true);
+  }
+
+  @Override
+  public void disconnect() {
+    setConnected(false);
+  }
+
+  @Override
+  public boolean isConnected() {
+    return super.isConnected();
+  }
+
+  // Status override
+  @Override
+  public String getStatus() {
+    String connStatus = isConnected() ? "up" : "down";
+    return "Thermostat[id=" + getId() + ", loc=" + getLocation() +
+           ", conn=" + connStatus + ", tempC=" + temperatureC + "]";
+  }
 }
+
